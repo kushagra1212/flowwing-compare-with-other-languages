f2454b4401a71bd8cbc67f9e80773930f9984379
kushagra1212 <kushagrarathore002@gmail.com>
2024-12-21 23:59:07 +0530

    initial commit

--- Benchmark.java
+++ Benchmark.java
@@ -1,8 +1,8 @@
 public class Benchmark {
     public static void main(String[] args) {
-        long limit = 10000;
-        long sum = 0;
-        for (long i = 1; i <= limit; i++) {
+        int limit = 10000;
+        int sum = 0;
+        for (int i = 1; i <= limit; i++) {
             sum += i;
         }
         System.out.println(sum);
